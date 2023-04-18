25712fb5ed14f0e6384dac085fd9e0c06fe7cd0b
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-04-18 18:02:54 +0300

    +

--- 01 Intro/lesson2.js
+++ 01 Intro/lesson2.js
@@ -2,14 +2,14 @@
 let x = 1;
 let y = 2;
 
-let res1 = " " + (x + y);
+let res1 = "" + (x + y);
 console.log(res1);
 console.log(typeof res1);
 
-let res2 = // Допишіть код, необхідно використовувати змінні x і y
-console.log(res2); // ""true2""
-console.log(typeof res2); // ""string""
-
+let res2 = true + "" + y;
+console.log(res2);
+console.log(typeof res2); 
+/*
 let res3 = // Допишіть код, необхідно використовувати змінні x і y
 console.log(res3); // true
 console.log(typeof res3); // ""boolean""
@@ -17,3 +17,4 @@ console.log(typeof res3); // ""boolean""
 let res4 = // Допишіть код, необхідно використовувати змінні x і y
 console.log(res4); // NaN
 console.log(typeof res4); // ""number""
+*/
