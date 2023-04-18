648555f1bfd85d15c95dca947f06df1a621a6182
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-04-18 18:14:42 +0300

    +

--- 01 Intro/lesson2.js
+++ 01 Intro/lesson2.js
@@ -1,15 +1,19 @@
-let res1 = "" + (x + y);
+// Завдання 2
+let w = 1;
+let v = 2;
+
+let res1 = "" + (w + v);
 console.log(res1);
 console.log(typeof res1);
 
-let res2 = true + "" + y;
+let res2 = true + "" + v;
 console.log(res2);
 console.log(typeof res2); 
 
-let res3 = (x < y);
+let res3 = (w < v);
 console.log(res3); 
 console.log(typeof res3);
 
-let res4 = y / 0;
+let res4 = v / 0;
 console.log(res4);
 console.log(typeof res4);
