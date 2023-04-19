a43b03c8c93b490cb84e80119fef9a373afd40f7
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-04-19 22:06:53 +0300

    +

--- 01 Intro/lesson2.js
+++ 01 Intro/lesson2.js
@@ -49,3 +49,16 @@ break;
 default:
      console.log("Доброго вечора");
 }
+
+let date = new Date();
+let hour = date.getHours();
+
+if (hour >= 23 || hour < 5) {
+    console.log("Доброї ночі");
+} else if (hour >= 5 && hour < 11) {
+    console.log("Доброго ранку");
+} else if (hour >= 11 && hour < 17) {
+    console.log("Доброго дня");
+} else {
+    console.log("Доброго вечора");
+}
