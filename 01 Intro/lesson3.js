3db8ad635b48d01c97a2a51f01e0f00c022708f9
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-04-22 22:09:21 +0300

    Add lesson3.js

---
+++ 01 Intro/lesson3.js
@@ -0,0 +1,13 @@
+// Завдання 1
+
+function createArray(start, end) {
+    let arr = [];
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+
+  // Завдання 2
+
+  
