4cc88dac95bc216add7494b342eee1b8e1640421
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-03 23:01:57 +0300

    +

--- Lesson 6/lesson6.js
+++ Lesson 6/lesson6.js
@@ -25,6 +25,7 @@ paragraphs[1].style.color = 'red';
 paragraphs[2].style.textDecoration = 'underline';
 paragraphs[3].style.fontStyle = 'italic';
 listItems.forEach(item => {
+item.style.whiteSpace = 'nowrap';
 item.style.display = 'inline-block';
 item.style.marginRight = '10px';
 });
