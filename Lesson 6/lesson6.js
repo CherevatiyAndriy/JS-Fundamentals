bd8c4f93fc98913d9260e62d2de5a9f84730fb70
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-03 22:07:35 +0300

    +

--- Lesson 6/lesson6.js
+++ Lesson 6/lesson6.js
@@ -8,3 +8,22 @@ function showModal() {
   function hideModal() {
     document.getElementById("myModal").style.display = "none";
   }
+
+  // Завдання 2
+  const header = document.querySelector('h1');
+const div = document.querySelector('#myDiv');
+const listItems = document.querySelectorAll('#myList li');
+const span = document.querySelector('span');
+header.style.fontSize = '48px';
+header.style.textAlign = 'center';
+div.style.backgroundColor = '#F5F5F5';
+div.style.border = '2px solid black';
+div.style.padding = '10px';
+div.style.display = 'flex';
+div.style.flexDirection = 'column';
+div.style.alignItems = 'center';
+listItems.forEach(item => {
+  item.style.display = 'inline-block';
+  item.style.marginRight = '10px';
+});
+span.style.display = 'none';
