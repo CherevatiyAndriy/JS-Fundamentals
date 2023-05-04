4b9d607d78d5c89427339b37c75e01f064917231
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-04 21:14:12 +0300

    +

--- Lesson 6/lesson6.js
+++ Lesson 6/lesson6.js
@@ -31,9 +31,9 @@ paragraphs[1].style.lineHeight = '1.5';
 paragraphs[2].style.lineHeight = '1.5';
 paragraphs[3].style.lineHeight = '1.5';
 listItems.forEach(item => {
-item.style.display = 'inline-block';
-item.style.marginRight = '10px';
+item.style.display = 'inline';
 item.style.whiteSpace = 'nowrap';
+item.style.marginRight = '10px';
 item.style.lineHeight = '1.5';
 });
 
