07e37023911b729454523065da66b8cef6def0d5
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-04 21:17:28 +0300

    +

--- Lesson 6/lesson6.js
+++ Lesson 6/lesson6.js
@@ -1,13 +1,13 @@
 // Завдання 1
-function showModal() {
-    var list = document.getElementById("list").getElementsByTagName("li");
-    var modalList = list[0].textContent + ", " + list[list.length - 1].textContent + ", " + list[1].textContent + ", " + list[3].textContent + ", " + list[2].textContent;
-    document.getElementById("modal-list").textContent = modalList;
-    document.getElementById("myModal").style.display = "block";
-  }
-  function hideModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
+// function showModal() {
+  //  var list = document.getElementById("list").getElementsByTagName("li");
+  //  var modalList = list[0].textContent + ", " + list[list.length - 1].textContent + ", " + list[1].textContent + ", " + list[3].textContent + ", " + list[2].textContent;
+  //  document.getElementById("modal-list").textContent = modalList;
+  //  document.getElementById("myModal").style.display = "block";
+  //}
+ // function hideModal() {
+ //   document.getElementById("myModal").style.display = "none";
+ // }
 
   // Завдання 2
 const header = document.querySelector('h1');
