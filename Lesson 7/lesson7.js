6b92dbd3e4d47000005a22926276ef3c222fad0e
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-07 16:30:53 +0300

    +

--- Lesson 7/lesson7.js
+++ Lesson 7/lesson7.js
@@ -16,39 +16,39 @@ setTimeout(() => {
 
 // Завдання 6
 
-document.addEventListener("DOMContentLoaded", function() {
-  window.addEventListener("resize", function() {
+function resize() {
   var width = window.innerWidth;
   var height = window.innerHeight;
   document.getElementById("width").textContent = width;
   document.getElementById("height").textContent = height;
-  });
-});
+  }
+  window.addEventListener("resize", resize);
+  resize();
 
 // Завдання 7
 
 const citiesByCountry = {
-  USA: ['New-York', 'Washington', 'Boston', 'Chikago'],
-  Ukraine: ['Kyiv', 'Ivano-Frankivs\'k', 'Lviv', 'Odesa'],
-  Germany: ['Berlin', 'Dresden', 'Hannover', 'Bonn'],
+    USA: ['New-York', 'Washington', 'Boston', 'Chikago'],
+    Ukraine: ['Kyiv', 'Ivano-Frankivs\'k', 'Lviv', 'Odesa'],
+    Germany: ['Berlin', 'Dresden', 'Hannover', 'Bonn'],
   };
-  const countrySelect = document.getElementById('country');
-  const citiesSelect = document.getElementById('cities');
-  const resultParagraph = document.querySelector('p');
-  countrySelect.addEventListener('change', () => {
-  const selectedCountry = countrySelect.value;
-  citiesSelect.innerHTML = '';
-  citiesByCountry[selectedCountry].forEach(city => {
-  const option = document.createElement('option');
-  option.value = city;
-  option.textContent = city;
-  citiesSelect.appendChild(option);
+const countrySelect = document.getElementById('country');
+const citiesSelect = document.getElementById('cities');
+const resultParagraph = document.querySelector('p');
+countrySelect.addEventListener('change', () => {
+const selectedCountry = countrySelect.value;
+citiesSelect.innerHTML = '';
+citiesByCountry[selectedCountry].forEach(city => {
+const option = document.createElement('option');
+option.value = city;
+option.textContent = city;
+citiesSelect.appendChild(option);
   });
-  const selectedCity = citiesSelect.value;
-  resultParagraph.textContent = `${selectedCountry} - ${selectedCity}`;
+const selectedCity = citiesSelect.value;
+resultParagraph.textContent = `${selectedCountry} - ${selectedCity}`;
   });
-  citiesSelect.addEventListener('change', () => {
-  const selectedCity = citiesSelect.value;
-  const selectedCountry = countrySelect.value;
-  resultParagraph.textContent = `${selectedCountry} - ${selectedCity}`;
+citiesSelect.addEventListener('change', () => {
+const selectedCity = citiesSelect.value;
+const selectedCountry = countrySelect.value;
+resultParagraph.textContent = `${selectedCountry} - ${selectedCity}`;
   });
