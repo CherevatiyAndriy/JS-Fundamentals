d96ef4987e151dd80863ce1f5417c7c2db4def8f
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-07 17:16:03 +0300

    +

--- Lesson 7/lesson7.js
+++ Lesson 7/lesson7.js
@@ -19,7 +19,28 @@ text.style.fontFamily = "Comic Sans MS";
 }
 
 // Завдання 3
-
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const link = document.getElementById('link');
+btn1.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'blue';
+});
+btn2.addEventListener('dblclick', () => {
+  document.body.style.backgroundColor = 'pink';
+});
+btn3.addEventListener('mousedown', () => {
+  document.body.style.backgroundColor = 'brown';
+});
+btn3.addEventListener('mouseup', () => {
+  document.body.style.backgroundColor = 'white';
+});
+link.addEventListener('mouseenter', () => {
+  document.body.style.backgroundColor = 'yellow';
+});
+link.addEventListener('mouseleave', () => {
+  document.body.style.backgroundColor = 'white';
+});
 
 // Завдання 6
 function resize() {
