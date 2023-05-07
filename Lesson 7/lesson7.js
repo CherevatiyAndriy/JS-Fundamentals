6e5d25e3e9b5512261301eee3a4445f2f3236a70
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-07 16:57:06 +0300

    +

--- Lesson 7/lesson7.js
+++ Lesson 7/lesson7.js
@@ -16,39 +16,37 @@ setTimeout(() => {
 
 // Завдання 6
 function resize() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    document.getElementById("width").textContent = width;
-    document.getElementById("height").textContent = height;
-    }
-    window.addEventListener("resize", resize);
-    resize();
-    
+var width = window.innerWidth;
+var height = window.innerHeight;
+document.getElementById("page-width").textContent = width;
+document.getElementById("height").textContent = height;
+  }
+window.addEventListener("resize", resize);
+resize();
+
 // Завдання 7
 const citiesByCountry = {
     USA: ['New-York', 'Washington', 'Boston', 'Chikago'],
     Ukraine: ['Kyiv', 'Ivano-Frankivs\'k', 'Lviv', 'Odesa'],
     Germany: ['Berlin', 'Dresden', 'Hannover', 'Bonn'],
-    };
-    const countrySelect = document.getElementById('country');
-    const citiesSelect = document.getElementById('cities');
-    const resultParagraph = document.getElementById('selected-result');
-    countrySelect.addEventListener('change', () => {
-    const selectedCountry = countrySelect.value;
-    citiesSelect.innerHTML = '';
-    citiesByCountry[selectedCountry].forEach(city => {
-    const option = document.createElement('option');
-    option.value = city;
-    option.textContent = city;
-    citiesSelect.appendChild(option);
-    });
-    updateSelectedResult(selectedCountry, '');
-    });
-    citiesSelect.addEventListener('change', () => {
-    const selectedCity = citiesSelect.value;
-    const selectedCountry = countrySelect.value;
-    updateSelectedResult(selectedCountry, selectedCity);
-    }); 
-    function updateSelectedResult(country, city) {
-    resultParagraph.textContent = country && city ? ${country} - ${city} : '';
-    }
+  };
+const countrySelect = document.getElementById('country');
+const citiesSelect = document.getElementById('cities');
+const resultParagraph = document.querySelector('p');
+countrySelect.addEventListener('change', () => {
+const selectedCountry = countrySelect.value;
+citiesSelect.innerHTML = '';
+citiesByCountry[selectedCountry].forEach(city => {
+const option = document.createElement('option');
+option.value = city;
+option.textContent = city;
+citiesSelect.appendChild(option);
+  });
+const selectedCity = citiesSelect.value;
+resultParagraph.textContent = `${selectedCountry} - ${selectedCity}`;
+  });
+citiesSelect.addEventListener('change', () => {
+const selectedCity = citiesSelect.value;
+const selectedCountry = countrySelect.value;
+document.getElementById('result').textContent = `${selectedCountry} - ${selectedCity}`;
+  });
