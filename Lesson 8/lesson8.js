610d29e4cb2c6e8ac68e90a6397b5608bee66cc9
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-07 20:49:17 +0300

    +

--- Lesson 8/lesson8.js
+++ Lesson 8/lesson8.js
@@ -1,12 +1,17 @@
 // Завдання 1
 function upperCase(str) {
     if (/^[A-Z]/.test(str)) {
-      return "String's starts with uppercase character";
+    return "String's starts with uppercase character";
     } else {
-      return "String's not starts with uppercase character";
+    return "String's not starts with uppercase character";
     }
   }
 console.log(upperCase('regexp'));
 console.log(upperCase('RegExp'));
 
 // Завдання 2
+function checkEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
+console.log(checkEmail("Qmail2@gmail.com"));
