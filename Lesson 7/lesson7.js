b7e487b57f29f5aaacf7307faf074017a97df689
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-07 16:52:05 +0300

    +

--- Lesson 7/lesson7.js
+++ Lesson 7/lesson7.js
@@ -16,13 +16,13 @@ setTimeout(() => {
 
 // Завдання 6
 function resize() {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  document.getElementById("width").textContent = width;
-  document.getElementById("height").textContent = height;
+var width = window.innerWidth;
+var height = window.innerHeight;
+document.getElementById("page-width").textContent = width;
+document.getElementById("height").textContent = height;
   }
-  window.addEventListener("resize", resize);
-  resize();
+window.addEventListener("resize", resize);
+resize();
 
 // Завдання 7
 const citiesByCountry = {
