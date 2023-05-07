daa70cb6e9357a6da6bb0af2fe0429d5a9c1e9b8
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-07 20:41:19 +0300

    +

---
+++ Lesson 8/lesson8.js
@@ -0,0 +1,12 @@
+// Завдання 1
+function upperCase(str) {
+    if (str[0] === str[0].toUpperCase()) {
+      return "String's starts with uppercase character";
+    } else {
+      return "String's not starts with uppercase character";
+    }
+  }
+console.log(upperCase('regexp'));
+console.log(upperCase('RegExp'));
+
+// Завдання 2
