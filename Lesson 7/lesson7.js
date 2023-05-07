dfe28464ebfbbd089f186eccd8762b59ccb674e6
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-07 15:33:41 +0300

    +

---
+++ Lesson 7/lesson7.js
@@ -0,0 +1,15 @@
+// Завдання 1
+
+const myWindow = window.open('', '', 'width=300,height=300');
+setTimeout(() => {
+  myWindow.resizeTo(500, 500);
+}, 2000);
+setTimeout(() => {
+  myWindow.moveTo(200, 200);
+}, 4000);
+setTimeout(() => {
+  myWindow.close();
+}, 6000);
+
+// Завдання 2
+
