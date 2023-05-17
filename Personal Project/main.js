a1694b2cbecccf5b67d2a4f046c7d577be76dba0
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-17 23:13:06 +0300

    +

--- Personal Project/main.js
+++ Personal Project/main.js
@@ -1,7 +1,6 @@
 const addForm = document.getElementById('addForm');
 const list = document.getElementById('list');
 const gallery = document.getElementById('gallery');
-
 class Shoe {
   constructor(name, brand, price, image) {
     this.name = name;
@@ -10,24 +9,19 @@ class Shoe {
     this.image = image;
   }
 }
-
 class Store {
   constructor() {
     this.shoes = [];
   }
-
   addShoe(shoe) {
     this.shoes.push(shoe);
   }
-
   removeShoe(index) {
     this.shoes.splice(index, 1);
   }
-
   displayShoes() {
     list.innerHTML = '';
     gallery.innerHTML = '';
-
     this.shoes.forEach((shoe, index) => {
       const listItem = document.createElement('li');
       listItem.innerHTML = `<span>${shoe.name}</span> - ${shoe.brand}, Ціна: ${shoe.price} грн`;
@@ -40,26 +34,14 @@ class Store {
       });
       listItem.appendChild(deleteBtn);
       list.appendChild(listItem);
-
-      const shoeDiv = document.createElement('div');
-      shoeDiv.className = 'shoeItem';
       const shoeImage = document.createElement('img');
       shoeImage.src = shoe.image;
       shoeImage.alt = shoe.name;
-      shoeDiv.appendChild(shoeImage);
-
-      const selectBtn = document.createElement('button');
-      selectBtn.className = 'selectBtn';
-      selectBtn.innerText = 'Вибрати';
-      shoeDiv.appendChild(selectBtn);
-
-      gallery.appendChild(shoeDiv);
+      gallery.appendChild(shoeImage);
     });
   }
 }
-
 const store = new Store();
-
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = document.getElementById('name').value;
