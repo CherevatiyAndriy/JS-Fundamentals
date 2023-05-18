190103043514e4ffbb4042e62ca9136378a6a70e
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-18 19:54:34 +0300

    +

--- Personal Project 2/main.js
+++ Personal Project 2/main.js
@@ -133,9 +133,6 @@ class User {
         professionSelect.value = '';
         professionError.style.display = 'none';
       })
-      .catch((error) => {
-        console.error(error);
-      });
   });
 // Ініціалізація
 loadUserDataFromLocalStorage();
