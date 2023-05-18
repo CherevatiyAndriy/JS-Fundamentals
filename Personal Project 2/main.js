c432d0a4f31dd5617013715b3a6d80c8b97605f9
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-18 19:49:28 +0300

    +

--- Personal Project 2/main.js
+++ Personal Project 2/main.js
@@ -1,91 +1,91 @@
 // Клас користувача
 class User {
-  constructor(surname, firstName, age, education, desiredPosition) {
-    this.surname = surname;
-    this.firstName = firstName;
-    this.age = age;
-    this.education = education;
-    this.desiredPosition = desiredPosition;
+    constructor(surname, firstName, age, education, desiredPosition) {
+      this.surname = surname;
+      this.firstName = firstName;
+      this.age = age;
+      this.education = education;
+      this.desiredPosition = desiredPosition;
+    }
   }
-}
-
-// Збереження даних користувача в локальному сховищі (LocalStorage)
-function saveUserDataLocally(user) {
-  const users = JSON.parse(localStorage.getItem('users')) || [];
-  users.push(user);
-  localStorage.setItem('users', JSON.stringify(users));
-}
-
-// Відправка даних користувача на сервер (симуляція запиту API)
-function sendUserDataToServer(user) {
-  return new Promise((resolve, reject) => {
-    // Симуляція асинхронного запиту на сервер
-    setTimeout(() => {
-      const success = Math.random() < 0.5; // 50% шанс успіху
-
-      if (success) {
-        console.log('Дані користувача успішно відправлено на сервер');
-        resolve();
-      } else {
-        console.error('Помилка при відправці даних на сервер');
-        reject();
-      }
-    }, 1000);
-  });
-}
-
-// Оновлення статусу мережі
-function updateNetworkStatus(online) {
-  const statusElement = document.getElementById('network-status');
-  if (statusElement) {
-    statusElement.textContent = online ? 'Онлайн' : 'Офлайн';
+  
+  // Збереження даних користувача в локальному сховищі (LocalStorage)
+  function saveUserDataLocally(user) {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
   }
-}
-
-// Перевірка доступу до мережі
-function checkNetworkStatus() {
-  return new Promise((resolve) => {
-    // Симуляція перевірки статусу мережі
-    const online = Math.random() < 0.8; // 80% шанс бути онлайн
-    resolve(online);
+  
+  // Відправка даних користувача на сервер (симуляція запиту API)
+  function sendUserDataToServer(user) {
+    return new Promise((resolve, reject) => {
+      // Симуляція асинхронного запиту на сервер
+      setTimeout(() => {
+        const success = Math.random() < 0.5; // 50% шанс успіху
+  
+        if (success) {
+          console.log('Дані користувача успішно відправлено на сервер');
+          resolve();
+        } else {
+          console.error('Помилка при відправці даних на сервер');
+          reject();
+        }
+      }, 1000);
+    });
+  }
+  
+  // Оновлення статусу мережі
+  function updateNetworkStatus(online) {
+    const statusElement = document.getElementById('network-status');
+    if (statusElement) {
+      statusElement.textContent = online ? 'Онлайн' : 'Офлайн';
+    }
+  }
+  
+  // Перевірка доступу до мережі
+  function checkNetworkStatus() {
+    return new Promise((resolve) => {
+      // Симуляція перевірки статусу мережі
+      const online = Math.random() < 0.8; // 80% шанс бути онлайн
+      resolve(online);
+    });
+  }
+  
+  // Завантаження даних з LocalStorage
+  function loadUserDataFromLocalStorage() {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    users.forEach((user) => {
+      renderUserData(user);
+    });
+  }
+  
+  // Рендеринг даних користувача на сторінці
+  function renderUserData(user) {
+    const userDataElement = document.createElement('div');
+    userDataElement.innerHTML = `
+      <p><strong>Прізвище:</strong> ${user.surname}</p>
+      <p><strong>Ім'я:</strong> ${user.firstName}</p>
+      <p><strong>Вік:</strong> ${user.age}</p>
+      <p><strong>Освіта:</strong> ${user.education}</p>
+      <p><strong>Бажана посада:</strong> ${user.desiredPosition}</p>
+      <hr>
+    `;
+    document.body.appendChild(userDataElement);
+  }
+  
+  // Оновлення статусу мережі при зміні
+  window.addEventListener('online', () => {
+    updateNetworkStatus(true);
+    loadUserDataFromLocalStorage();
   });
-}
-
-// Завантаження даних з LocalStorage
-function loadUserDataFromLocalStorage() {
-  const users = JSON.parse(localStorage.getItem('users')) || [];
-  users.forEach((user) => {
-    renderUserData(user);
+  
+  // Оновлення статусу мережі при зміні
+  window.addEventListener('offline', () => {
+    updateNetworkStatus(false);
   });
-}
-
-// Рендеринг даних користувача на сторінці
-function renderUserData(user) {
-  const userDataElement = document.createElement('div');
-  userDataElement.innerHTML = `
-    <p><strong>Прізвище:</strong> ${user.surname}</p>
-    <p><strong>Ім'я:</strong> ${user.firstName}</p>
-    <p><strong>Вік:</strong> ${user.age}</p>
-    <p><strong>Освіта:</strong> ${user.education}</p>
-    <p><strong>Бажана посада:</strong> ${user.desiredPosition}</p>
-    <hr>
-  `;
-  document.body.appendChild(userDataElement);
-}
-
-// Оновлення статусу мережі при зміні
-window.addEventListener('online', () => {
-  updateNetworkStatus(true);
-  loadUserDataFromLocalStorage();
-});
-
-// Оновлення статусу мережі при зміні
-window.addEventListener('offline', () => {
-  updateNetworkStatus(false);
-});
-
-// Обробка події надсилання форми
-document.getElementById('registration-form').addEventListener('submit', (event) => {
+  
+  // Обробка події надсилання форми
+  document.getElementById('registration-form').addEventListener('submit', (event) => {
     event.preventDefault(); // Зупинка стандартної поведінки форми
   
     const surname = document.getElementById('surname').value;
@@ -99,46 +99,42 @@ document.getElementById('registration-form').addEventListener('submit', (event) 
     const selectedProfession = professionSelect.value;
   
     const availableProfessions = ['Лікар', 'Вчитель', 'Інженер', 'Актор', 'Програміст', 'Дизайнер'];
-  
     if (!availableProfessions.includes(selectedProfession)) {
-      // Перевірка, чи користувач ввів свою професію
-      if (selectedProfession.trim() !== '') {
-        // Додавання користувачевої професії до списку доступних
-        availableProfessions.push(selectedProfession);
-      } else {
-        // Показ повідомлення про необхідність ввести професію
-        const professionError = document.getElementById('profession-error');
-        professionError.textContent = 'Введіть вашу професію';
-        professionError.style.display = 'block';
-        return; // Припинити виконання функції
-      }
+      const professionError = document.getElementById('profession-error');
+      professionError.style.display = 'block';
+      return; // Зупинка виконання функції при недійсній професії
     }
   
-    // Якщо обрана професія є у списку доступних, продовжити обробку форми
+    // Створення об'єкту користувача
     const user = new User(surname, firstName, age, education, desiredPosition);
   
+    // Перевірка доступу до мережі
     checkNetworkStatus()
       .then((online) => {
         if (online) {
+          // Відправка даних користувача на сервер
           return sendUserDataToServer(user);
         } else {
+          // Збереження даних користувача локально
           saveUserDataLocally(user);
+          throw new Error('Користувача збережено локально. Перевірте підключення до мережі для відправки на сервер.');
         }
       })
       .then(() => {
-        // Очищення полів форми
+        // Рендеринг даних користувача на сторінці
+        renderUserData(user);
+  
+        // Очистка полів форми
         document.getElementById('surname').value = '';
         document.getElementById('firstName').value = '';
         document.getElementById('age').value = '';
         document.getElementById('education').value = '';
         document.getElementById('desiredPosition').value = '';
         professionSelect.value = '';
-  
-        // Видалення даних з LocalStorage
-        localStorage.removeItem('users');
+        professionError.style.display = 'none';
       })
-      .catch(() => {
-        console.error('Помилка при обробці даних');
+      .catch((error) => {
+        console.error(error);
       });
   });
 // Ініціалізація
