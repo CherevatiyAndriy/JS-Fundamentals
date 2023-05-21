499883eff19b1f8ff53d4cb6a0cc1b1a25b63e41
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-21 21:18:51 +0300

    +

--- Lesson 10/main.js
+++ Lesson 10/main.js
@@ -41,3 +41,20 @@ console.log(mul(1, "str", 2, 3, true));
 console.log(mul(null, "str", false, true));
 
 // Завдання 4
+
+function mapBuilder(keysArray, valuesArray) {
+if (keysArray.length !== valuesArray.length) {
+throw new Error("Довжина масивів keysArray та valuesArray повинна бути однаковою.");
+}
+const map = new Map();
+for (let i = 0; i < keysArray.length; i++) {
+map.set(keysArray[i], valuesArray[i]);
+}
+return map;
+}
+
+let keys = [1, 2, 3, 4];
+let values = ["div", "span", "b", "i"];
+let map = mapBuilder(keys, values);
+console.log(map.size);
+console.log(map.get(2));
