e6658b522bc70d15e942009156dfe25cb246ef22
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-21 21:09:39 +0300

    +

---
+++ Lesson 10/main.js
@@ -0,0 +1,16 @@
+// Завдання 1 
+
+let names = {
+    first: "Tom",
+    second: "Sam",
+    third: "Ray",
+    fourth: "Bob",
+};
+let { first: f, third: x, fifth = "Name №5" } = names;
+
+console.log(f);
+console.log(x);
+console.log(fifth);
+
+// Завдання 2
+
