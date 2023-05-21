a189e101f7e224bf9ae24d1fc3abd65a58425585
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-21 21:28:35 +0300

    +

--- Lesson 11/main.js
+++ Lesson 11/main.js
@@ -14,3 +14,20 @@ console.log(data);
 
 // Завдання 2
 
+function calcArrProduct(arr) {
+return new Promise(function(resolve, reject) {
+let product = 1;
+for (let i = 0; i < arr.length; i++) {
+if (typeof arr[i] === "number") {
+product *= arr[i];
+} else {
+reject("Error! Incorrect array!");
+return;
+}
+}
+resolve(product);
+});
+}
+
+calcArrProduct([3, 4, 5]).then(result => console.log(result));
+calcArrProduct([5, "user2", 7, 12]).then(result => console.log(result));
