886f48f7f2ad23f7aa9cfaf25ab1b5b2e370ad1d
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-28 23:36:38 +0300

    +

--- Personal Project 2/main.js
+++ Personal Project 2/main.js
@@ -82,30 +82,37 @@ function hideError() {
 }
 
 function updateDesiredPosition(selectElement) {
-  const desiredPositionInput = document.getElementById('desiredPosition');
-  if (desiredPositionInput) {
-    desiredPositionInput.value = selectElement.value;
+  const desiredPositionElement = document.getElementById('desiredPosition');
+  if (desiredPositionElement) {
+    desiredPositionElement.value = selectElement.value;
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  loadUserDataFromLocalStorage();
-});
+const professions = [
+  "Лікар",
+  "Вчитель",
+  "Інженер",
+  "Актор",
+  "Програміст",
+  "Дизайнер",
+  "Повар",
+  "Бармен",
+  "Адміністратор",
+  "Викладач англійської мови",
+  "Економіст",
+  "Юрист",
+  "Музикант",
+  "Пекар",
+  "Продавець-консультант"
+];
 
-window.addEventListener('beforeunload', () => {
-  const surname = document.getElementById('surname').value;
-  const firstName = document.getElementById('firstName').value;
-  const age = document.getElementById('age').value;
-  const education = document.getElementById('education').value;
-  const contact = document.getElementById('contact').value;
-  const desiredPosition = document.getElementById('desiredPosition').value;
-
-  const user = new User(surname, firstName, age, education, contact, desiredPosition);
-  saveUserDataLocally(user);
-});
+function validateProfession(profession) {
+  return professions.includes(profession.trim());
+}
 
 window.addEventListener('online', () => {
   updateNetworkStatus(true);
+  loadUserDataFromLocalStorage();
 });
 
 window.addEventListener('offline', () => {
@@ -161,3 +168,4 @@ document.getElementById('registration-form').addEventListener('submit', async (e
 });
 
 updateNetworkStatus(navigator.onLine);
+loadUserDataFromLocalStorage();
