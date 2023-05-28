ee376002c7bbd52993f3fbdf4fbd3c4365ad2e6e
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-28 23:15:08 +0300

    +

--- Personal Project 2/main.js
+++ Personal Project 2/main.js
@@ -1,11 +1,12 @@
 class User {
-  constructor(surname, firstName, age, education, contact, desiredPosition) {
+  constructor(surname, firstName, age, education, contact, desiredPosition, additionalData) {
     this.surname = surname;
     this.firstName = firstName;
     this.age = age;
     this.education = education;
     this.contact = contact;
     this.desiredPosition = desiredPosition;
+    this.additionalData = additionalData;
   }
 }
 
@@ -65,6 +66,11 @@ function renderUserData(user) {
     <p><strong>Освіта:</strong> ${user.education}</p>
     <p><strong>Контактні дані:</strong> ${user.contact}</p>
     <p><strong>Бажана професія:</strong> ${user.desiredPosition}</p>
+    <p><strong>Додаткові дані:</strong></p>
+    <ul>
+      <li><strong>Додаткове поле 1:</strong> ${user.additionalData.additionalField1}</li>
+      <li><strong>Додаткове поле 2:</strong> ${user.additionalData.additionalField2}</li>
+    </ul>
     <hr>
   `;
   document.body.appendChild(userDataElement);
@@ -149,7 +155,17 @@ document.getElementById('registration-form').addEventListener('submit', async (e
     return;
   }
 
-  const user = new User(surname, firstName, age, education, contact, desiredPosition);
+  // Отримання додаткових даних з форми
+  const additionalField1 = document.getElementById('additionalField1').value;
+  const additionalField2 = document.getElementById('additionalField2').value;
+
+  const additionalData = {
+    additionalField1,
+    additionalField2
+  };
+
+  const user = new User(surname, firstName, age, education, contact, desiredPosition, additionalData);
+  
   try {
     const online = await checkNetworkStatus();
 
