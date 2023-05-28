49782e3be8a59aa32a5e3a2573bbef35291dfced
CherevatiyAndriy <a_Slim_13@ukr.net>
2023-05-28 18:23:04 +0300

    +

--- Personal Project 2/main.js
+++ Personal Project 2/main.js
@@ -120,6 +120,7 @@ document.getElementById('registration-form').addEventListener('submit', async (e
 
   if (!validateProfession(desiredPosition)) {
     const professionSelect = document.getElementById('profession');
+    professionSelect.disabled = false;
     professionSelect.required = true;
     professionSelect.focus();
 
@@ -144,9 +145,6 @@ document.getElementById('registration-form').addEventListener('submit', async (e
     renderUserData(user);
     hideError();
     document.getElementById('registration-form').reset();
-    
-    const professionSelect = document.getElementById('profession');
-    professionSelect.disabled = true; // Встановлюємо знову значення `disabled`
   } catch (error) {
     console.error(error);
     showError();
